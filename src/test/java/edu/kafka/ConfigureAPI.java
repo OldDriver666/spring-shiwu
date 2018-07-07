668980b3c17ee231f9b28011719a8d7fe352131d
tany <jayi1314@163.com>
2018-07-07 11:56:25 +0800

    kafka测试

---
+++ src/test/java/edu/kafka/ConfigureAPI.java
@@ -0,0 +1,21 @@
+package edu.kafka;
+ 
+public class ConfigureAPI
+{
+ 
+    public final static String GROUP_ID = "test";
+ 
+    public final static String TOPIC = "test";
+ 
+    public final static int BUFFER_SIZE = 64 * 1024;
+ 
+    public final static int TIMEOUT = 20000;
+ 
+    public final static int INTERVAL = 10000;
+ 
+    public final static String BROKER_LIST = "192.168.245.131:9092";
+ 
+    // 去数据间隔
+    public final static int GET_MEG_INTERVAL = 1000;
+ 
+}
---
+++ src/test/java/edu/kafka/JProducer.java
@@ -0,0 +1,64 @@
+package edu.kafka;
+ 
+ 
+import java.util.Properties;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+ 
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+ 
+ 
+public class JProducer implements Runnable
+{
+    private Producer<String, String> producer;
+ 
+    public JProducer()
+    {
+        Properties props = new Properties();
+ 
+        props.put("bootstrap.servers", ConfigureAPI.BROKER_LIST);
+        props.put("acks", "all");
+        props.put("retries", 0);
+        props.put("batch.size", 16384);
+        props.put("linger.ms", 1);
+        props.put("buffer.memory", 33554432);
+        props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        props.put("request.required.acks", "-1");
+ 
+        producer = new KafkaProducer<String, String>(props);
+ 
+    }
+ 
+    @Override
+    public void run()
+    {
+        // TODO Auto-generated method stub
+        try
+        {
+            String data = "hello lmx!";
+            producer.send(new ProducerRecord<String, String>(ConfigureAPI.TOPIC, data));
+            System.out.println(data);
+        }
+        catch (Exception e)
+        {
+            // TODO: handle exception
+            e.getStackTrace();
+        }
+        finally
+        {
+            producer.close();
+        }
+ 
+    }
+ 
+    public static void main(String[] args)
+    {
+        ExecutorService threadPool = Executors.newCachedThreadPool();
+        threadPool.execute(new JProducer());
+        threadPool.shutdown();
+    }
+ 
+}
